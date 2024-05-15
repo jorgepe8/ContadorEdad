e70976fb0304522b1391b44e194fdbbf835f2f9c
jorgepe8 <jorgelico01@gmail.com>
2024-05-15 22:30:11 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    var fechaNacimiento = new Date('2001-04-12'); // Reemplaza YYYY-MM-DD con tu fecha de nacimiento
+    var intervalo = setInterval(actualizarEdad, 100);
+  
+    function actualizarEdad() {
+      var ahora = new Date();
+      var edad = (ahora - fechaNacimiento) / (1000 * 60 * 60 * 24 * 365.25); // Calcula la edad en años con decimales
+      document.getElementById('edad').textContent = edad.toFixed(9); // Muestra la edad con 9 decimales
+    }
+  };
